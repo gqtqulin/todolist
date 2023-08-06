dfaff04c556092a1f5ca84c79bb1b7b93bb3b2f7
gqtqulin <gqtqulin@icloud.com>
2023-08-06 17:06:05 +0300

    list and add func created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { MouseEventHandler } from "react";
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+type Task = {
+  id: number;
+  title: string;
+  desc: string;
+  status: boolean;
+};
 
 function App() {
+  const [tasks, setTasks] = useState<Array<Task>>([]);
+
+  const addTask = (title: string, desc: string): void => {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now(),
+        title: title,
+        desc: desc,
+        status: false,
+      },
+    ]);
+  };
+
+  const deleteTask = (id: number) => {
+    const taskIndexToDelete: number = tasks.findIndex((t) => t.id === id);
+    if (tasks[taskIndexToDelete] == undefined)
+      throw new Error("Задача для удаления не найдена");
+    tasks.splice(taskIndexToDelete, 1);
+  };
+
+  const updateTask = () => {};
+
+  const changeTaskStatus = () => {};
+
+  //handlers
+  const handleAddTaskClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    addTask(title1, desc1);
+  };
+
+  const handleDeleteTaskClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+  };
+
+  // 
+
+  //test task
+  const title1: string = 'title1';
+  const desc1: string = 'desc1';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>privet ts react app</h1>
+
+      <button onClick={handleAddTaskClick}>add task</button>
+
+      <p>{JSON.stringify(tasks)}</p>
+
+      <form>
+        
+      </form>
     </div>
   );
 }
